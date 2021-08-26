a83a723b53c2aaf1450108bdaa980f5f329b9f23
jmpark <pok_nima@naver.com>
2021-08-26 10:06:11 +0900

    test() 모듈 완료

--- src/hellogit/HelloWorld.java
+++ src/hellogit/HelloWorld.java
@@ -6,6 +6,11 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("지금 git 수업 중");
 		System.out.println("집중 하세요~~~ 단 한 번만 수업합니다.");
+		test();
+	}
+	
+	public static void test() {
+		System.out.println("새 모듈 작업 완성.");
 	}
 
 }
